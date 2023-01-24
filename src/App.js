5b9d7f0d0554ca58e9a53e78bbb813657f095e54
Namyunha <108773845+Namyunha@users.noreply.github.com>
2023-01-24 13:20:10 +0900

    Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,50 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-    console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const form = (e) => {
+    e.preventDefault();
   };
-  const onDelete = (e) => {
-    const element = e.target.id;
-    console.log(e.target.id);
-    setToDos((currentArray) =>
-      currentArray.filter((item) => {
-        return item !== element;
-      })
+  const onCalculate = (e) => {
+    const result = coins.filter(
+      (coin) => coin.quotes.USD.price < e.target.value
     );
+    setCoins(result);
   };
   return (
     <div>
-      <h1>My To Dos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do...."
-        />
-        <button>Add To Do</button>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`} </h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.rank}.{coin.name} ({coin.symbol}: ($
+              {Math.floor(coin.quotes.USD.price)}
+              ))
+            </option>
+          ))}
+        </select>
+      )}
+      <form onSubmit={form} htmlFor={"pay"}>
+        <input onChange={onCalculate} id="pay" name="pay" type={"text"} />
+        <button>Search</button>
       </form>
-      <hr />
-      <ul>
-        {toDos.reverse().map((item, index) => (
-          <li key={index}>
-            {item}
-            <button onClick={onDelete} id={item}>
-              ❌
-            </button>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
